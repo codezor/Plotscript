80d1bc1f879dd4b7e27501a5dc4cb95baff8dfbf
Cody Crofford <ccody7@vt.edu>
2018-09-04 11:09:12 -0400

    Task 3 Milestone zero symbols and functions added

--- environment.cpp
+++ environment.cpp
@@ -6,7 +6,7 @@
 #include "environment.hpp"
 #include "semantic_error.hpp"
 
-/*********************************************************************** 
+/***********************************************************************
 Helper Functions
 **********************************************************************/
 
@@ -15,7 +15,7 @@ bool nargs_equal(const std::vector<Expression> & args, unsigned nargs){
   return args.size() == nargs;
 }
 
-/*********************************************************************** 
+/***********************************************************************
 Each of the functions below have the signature that corresponds to the
 typedef'd Procedure function pointer.
 **********************************************************************/
@@ -32,7 +32,7 @@ Expression add(const std::vector<Expression> & args){
   double result = 0;
   for( auto & a :args){
     if(a.isHeadNumber()){
-      result += a.head().asNumber();      
+      result += a.head().asNumber();
     }
     else{
       throw SemanticError("Error in call to add, argument not a number");
@@ -43,12 +43,12 @@ Expression add(const std::vector<Expression> & args){
 };
 
 Expression mul(const std::vector<Expression> & args){
- 
+
   // check all aruments are numbers, while multiplying
   double result = 1;
   for( auto & a :args){
     if(a.isHeadNumber()){
-      result *= a.head().asNumber();      
+      result *= a.head().asNumber();
     }
     else{
       throw SemanticError("Error in call to mul, argument not a number");
@@ -75,7 +75,7 @@ Expression subneg(const std::vector<Expression> & args){
     if( (args[0].isHeadNumber()) && (args[1].isHeadNumber()) ){
       result = args[0].head().asNumber() - args[1].head().asNumber();
     }
-    else{      
+    else{
       throw SemanticError("Error in call to subtraction: invalid argument.");
     }
   }
@@ -88,13 +88,13 @@ Expression subneg(const std::vector<Expression> & args){
 
 Expression div(const std::vector<Expression> & args){
 
-  double result = 0;  
+  double result = 0;
 
   if(nargs_equal(args,2)){
     if( (args[0].isHeadNumber()) && (args[1].isHeadNumber()) ){
       result = args[0].head().asNumber() / args[1].head().asNumber();
     }
-    else{      
+    else{
       throw SemanticError("Error in call to division: invalid argument.");
     }
   }
@@ -103,9 +103,142 @@ Expression div(const std::vector<Expression> & args){
   }
   return Expression(result);
 };
+// Sqrt Procedure : Square root procedure unary
+Expression sqrt(const std::vector<Expression> & args ) {
+
+  double result = 0;
+  // preconditions
+  if (nargs_equal(args,1)){
+
+    if (args[0].isHeadNumber()){
+
+      if(args[0].head().asNumber() >= 0 ){
+        result =  sqrt(args[0].head().asNumber());
+      }
+
+      else {
+        throw SemanticError("Error in call to square root: Negitive arguments");
+      }
+    }
+    else {
+      throw SemanticError("Error in call to square root: invalid argument.");
+    }
+  }
+  else {
+    throw SemanticError("Error in call to square root: invalid number of arguments.");
+  }
+  return Expression(result);
+};
+//  exponential Procedure: biniary procedure
+Expression exp(const std::vector<Expression> & args){
+
+  double result = 0;
+
+  //preconditions
+  if(nargs_equal(args,2)){
+    if( (args[0].isHeadNumber()) && (args[1].isHeadNumber())) {
+      result = std::pow(args[0].head().asNumber(), args[1].head().asNumber());
+    }
+    else{
+      throw SemanticError("Error in call to exponential: invalid argument.");
+    }
+
+  }
+  else{
+    throw SemanticError("Error in call to exponential: invalid number of arguments.");
+  }
+
+  return Expression(result);
+};
+
+// Natural Logarithm Procedure: Natural Log unary
+Expression ln(const std::vector<Expression> & args){
+
+  double result =0;
+
+  // preconditions
+  if(nargs_equal(args,1)){
+
+    if(args[0].isHeadNumber()){
+      result = std::log(args[0].head().asNumber());
+    }
+    else{
+      throw SemanticError("Error in call to natural log: invalid argument.");
+    }
+  }
+  else{
+    throw SemanticError("Error in call to natural log: invalid number of arguments.");
+  }
+
+  return Expression(result);
+};
+
+// Sin Procedure:
+Expression sin(const std::vector<Expression> & args){
+  double result =0;
+
+  //preconditions
+  if(nargs_equal(args,1)){
+
+    if(args[0].isHeadNumber()){
+      result = std::sin(args[0].head().asNumber());
+    }
+    else{
+      throw SemanticError("Error in call to sin: invalid argument.");
+    }
+  }
+  else{
+    throw SemanticError("Error in call to sin: invalid number of arguments.");
+  }
+  return Expression(result);
+
+};
+
+// Cos Procedure:
+Expression cos(const std::vector<Expression> & args){
+  double result =0;
+
+  //preconditions
+  if(nargs_equal(args,1)){
+
+    if(args[0].isHeadNumber()){
+      result = std::cos(args[0].head().asNumber());
+    }
+    else{
+      throw SemanticError("Error in call to cos: invalid argument.");
+    }
+  }
+  else{
+    throw SemanticError("Error in call to cose: invalid number of arguments.");
+  }
+  return Expression(result);
+
+};
+
+// Tan Procedure:
+Expression tan(const std::vector<Expression> & args){
+  double result =0;
+
+  //preconditions
+  if(nargs_equal(args,1)){
+
+    if(args[0].isHeadNumber()){
+      result = std::tan(args[0].head().asNumber());
+    }
+    else{
+      throw SemanticError("Error in call to tan: invalid argument.");
+    }
+  }
+  else{
+    throw SemanticError("Error in call to tan: invalid number of arguments.");
+  }
+  return Expression(result);
+
+};
 
 const double PI = std::atan2(0, -1);
 const double EXP = std::exp(1);
+//const double I = std::complex<double> myComplex;
 
 Environment::Environment(){
 
@@ -114,13 +247,13 @@ Environment::Environment(){
 
 bool Environment::is_known(const Atom & sym) const{
   if(!sym.isSymbol()) return false;
-  
+
   return envmap.find(sym.asSymbol()) != envmap.end();
 }
 
 bool Environment::is_exp(const Atom & sym) const{
   if(!sym.isSymbol()) return false;
-  
+
   auto result = envmap.find(sym.asSymbol());
   return (result != envmap.end()) && (result->second.type == ExpressionType);
 }
@@ -128,7 +261,7 @@ bool Environment::is_exp(const Atom & sym) const{
 Expression Environment::get_exp(const Atom & sym) const{
 
   Expression exp;
-  
+
   if(sym.isSymbol()){
     auto result = envmap.find(sym.asSymbol());
     if((result != envmap.end()) && (result->second.type == ExpressionType)){
@@ -144,18 +277,18 @@ void Environment::add_exp(const Atom & sym, const Expression & exp){
   if(!sym.isSymbol()){
     throw SemanticError("Attempt to add non-symbol to environment");
   }
-    
+
   // error if overwriting symbol map
   if(envmap.find(sym.asSymbol()) != envmap.end()){
     throw SemanticError("Attempt to overwrite symbol in environemnt");
   }
 
-  envmap.emplace(sym.asSymbol(), EnvResult(ExpressionType, exp)); 
+  envmap.emplace(sym.asSymbol(), EnvResult(ExpressionType, exp));
 }
 
 bool Environment::is_proc(const Atom & sym) const{
   if(!sym.isSymbol()) return false;
-  
+
   auto result = envmap.find(sym.asSymbol());
   return (result != envmap.end()) && (result->second.type == ProcedureType);
 }
@@ -181,19 +314,45 @@ then re-add the default ones.
 void Environment::reset(){
 
   envmap.clear();
-  
+
   // Built-In value of pi
   envmap.emplace("pi", EnvResult(ExpressionType, Expression(PI)));
 
+  // Added Built in Eulers number
+  envmap.emplace("e", EnvResult(ExpressionType, Expression(std::exp(1))));
+
+  // Add Built in Symbol I: Complex numer I =sqrt(-1) = 0 + i
+  //envmap.emplace("I", EnvResult(ExpressionType, Expression(std::complex<double>));
+
   // Procedure: add;
-  envmap.emplace("+", EnvResult(ProcedureType, add)); 
+  envmap.emplace("+", EnvResult(ProcedureType, add));
 
   // Procedure: subneg;
-  envmap.emplace("-", EnvResult(ProcedureType, subneg)); 
+  envmap.emplace("-", EnvResult(ProcedureType, subneg));
 
   // Procedure: mul;
-  envmap.emplace("*", EnvResult(ProcedureType, mul)); 
+  envmap.emplace("*", EnvResult(ProcedureType, mul));
 
   // Procedure: div;
-  envmap.emplace("/", EnvResult(ProcedureType, div)); 
+  envmap.emplace("/", EnvResult(ProcedureType, div));
+
+  //Added Built-in Procedure sqrt
+  envmap.emplace("sqrt", EnvResult(ProcedureType, sqrt));
+
+  //Added Built-in Procedure exponential
+  envmap.emplace("^", EnvResult(ProcedureType, exp));
+
+  //Added Built-in Procedure ln
+  envmap.emplace("ln", EnvResult(ProcedureType, ln));
+
+  //Added Built in Procuedure sin
+  envmap.emplace("sin", EnvResult(ProcedureType, sin));
+
+  //Added Built in Procuedure cos
+  envmap.emplace("cos", EnvResult(ProcedureType, cos));
+
+  //Added Built in Procuedure tan
+  envmap.emplace("tan", EnvResult(ProcedureType, tan));
+
+
 }

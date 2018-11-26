a3842c17d2e96edb6ba17e9a81a6ca421450073a
Cody Crofford <ccody7@vt.edu>
2018-11-25 21:54:02 -0500

    Adds a start and stop functions

--- plotscript.cpp
+++ plotscript.cpp
@@ -47,21 +47,21 @@ void EvalOne(Interpreter& interp, std::istringstream& expression)
 	if(!interp.parseStream(expression))
 	{
 		error("Invalid Expression. Could not parse.");
-		//return EXIT_FAILURE;
+		
 	}
 	else
 	{
 		try
 		{
 			Expression exp = interp.evaluate();
+			std::cout << exp;
 			output.push(exp);
-			//std::cout << exp << std::endl;
-			//return exp;
+			
 		}
 		catch(const SemanticError& ex)
 		{
 			std::cerr << ex.what() << std::endl;
-			//return EXIT_FAILURE;
+			
 		}
 	}
 }
@@ -109,8 +109,10 @@ eval_from_stream(std::istream& stream, Interpreter& interp)
 }
 
 int
-eval_from_file(std::string filename, Interpreter& interp)
+eval_from_file(std::string filename)
 {
+	Interpreter interp;
+	startUp(interp);
   std::ifstream ifs(filename);
 
   if (!ifs) {
@@ -121,15 +123,17 @@ eval_from_file(std::string filename, Interpreter& interp)
 }
 
 int
-eval_from_command(std::string argexp, Interpreter& interp)
+eval_from_command(std::string argexp)
 {	
+	Interpreter interp;
+	startUp(interp);
   std::istringstream expression(argexp);
 
   return eval_from_stream(expression, interp);
 }
 
 // Im sure I shouln't do this 
-int eval_from_message_queue(message_queue<std::string> &queue, Interpreter& interp)
+int eval_from_message_queue(message_queue<std::string> &queue)
 {
 	while(!queue.empty())
 	{
@@ -137,7 +141,7 @@ int eval_from_message_queue(message_queue<std::string> &queue, Interpreter& inte
 		//std::string const string = 
 		queue.wait_and_pop(string);
 		// Eval from command uses eval from stream. Eval from stream instantiates a new interpereter.
-		int const result = eval_from_command(string, interp);
+		int const result = eval_from_command(string);
 		if(result != EXIT_SUCCESS)
 		{
 			return result;
@@ -156,6 +160,7 @@ void plotscript_thread_main()
 
 	Interpreter interp;
 	startUp(interp);
+
 	while(true)
 	{
 		//prompt();
@@ -164,11 +169,9 @@ void plotscript_thread_main()
 
 		if(line.empty())
 			continue;
-		
-		//std::string plotscript_program;
-		//input.wait_and_pop(plotscript_program);	
-		std::istringstream expression(line);
-		EvalOne(interp, expression);
+			
+		std::istringstream expression(line);		
+		EvalOne(interp, expression);		
 	}
 }
 
@@ -227,7 +230,8 @@ repl()
 
 			if(kernalThread == nullptr)
 			{
-				kernalThread = new std::thread(plotscript_thread_main);				
+				kernalThread = new std::thread(plotscript_thread_main);	
+				continue;
 			}		
 				
 		}
@@ -238,14 +242,19 @@ repl()
 			if(kernalThread == nullptr)
 			{
 				error("Interpreter kernel not running");
+				continue;
 			}
 			else
 			{
 				input.push(line);
 				while(output.empty())
 				{
-
+					if(input.empty())
+					{
+						break;
+					}
 				}
+				continue;
 			}
 
 		}		
@@ -259,13 +268,12 @@ repl()
 int
 main(int argc, char* argv[])
 {	
-	Interpreter interp;
-	startUp(interp);
+	
 
   if (argc == 2) {
 	 
 	  // this will parse and evaluate
-    return eval_from_file(argv[1], interp);
+    return eval_from_file(argv[1]);
   
   } 
  
@@ -274,7 +282,7 @@ main(int argc, char* argv[])
 
     if (std::string(argv[1]) == "-e") {
 		// this will parse and evaluate 
-      return eval_from_command(argv[2], interp);
+      return eval_from_command(argv[2]);
     } 
 	else {
       error("Incorrect number of command line arguments.");
@@ -284,7 +292,8 @@ main(int argc, char* argv[])
  
   else {		
 		
-		repl();		
+		std::thread Mainthread(repl);
+		Mainthread.join();
 	
   }
  
